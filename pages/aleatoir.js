2a677d484d2dbc50a2fe923e44ce0bd8436be11d
samiakhalfi1718 <samia.khalfi.1718@gmail.com>
2023-05-31 14:21:21 +0100

    update project

--- pages/aleatoir.js
+++ pages/aleatoir.js
@@ -1,12 +1,9 @@
 exports.AleatoirPage = class AleatoirPage {
-    
 
     constructor(page) {
         this.page = page
         this.aleatoir_buttom = page.locator("xpath=//*[contains(text(),'Aléatoire')]")
     }
-
-
 
     async toConnect() {
         await this.page.goto('https://bouletcorp.com/');
@@ -15,8 +12,17 @@ exports.AleatoirPage = class AleatoirPage {
     async clickOnAleatoir() {
         await this.aleatoir_buttom.first().click();
     }
+    async getCurrentURL () {
+        const mmm=window.location.href
+        console.log(mmm)
+        return mmm
+      }
 
     async checkUrlChanged() {
+        const title = getCurrentURL()
+        console.log(title);
+       // if title.e('https://bouletcorp.com/');
+             
     }
 
     
--- tests/aleatoir.spec.js
+++ tests/aleatoir.spec.js
@@ -8,12 +8,16 @@ test('test', async ({ page }) => {
     await Ale.toConnect()
     //await page.waitForTimeout(30000)
     await Ale.clickOnAleatoir()
+    await Ale.checkUrlChanged
+
+
+
     //await page.waitForTimeout(10000)
     const facebookWidget = page.locator('div:nth-child(5) > #notes > #small_nav > #social_network > .socialize-this > li:nth-child(3) > a')
     const twitterWidget = page.locator('div:nth-child(5) > #notes > #small_nav > #social_network > .socialize-this > li:nth-child(4) > a')
     const tumblrWidget = page.locator('div:nth-child(5) > #notes > #small_nav > #social_network > .socialize-this > li:nth-child(5) > a')
 
-    facebookWidget.isVisible
-    twitterWidget.isVisible
-    tumblrWidget.isVisible
+    expect(facebookWidget).toBeTruthy();
+    expect(twitterWidget).toBeTruthy();
+    expect(tumblrWidget).toBeTruthy();
 });
