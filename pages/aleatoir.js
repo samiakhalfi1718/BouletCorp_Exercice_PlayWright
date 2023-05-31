41f15b663823a0bc2b40659b3b26725222c53639
samiakhalfi1718 <samia.khalfi.1718@gmail.com>
2023-05-31 11:03:56 +0100

    new project playwright

---
+++ pages/aleatoir.js
@@ -0,0 +1,23 @@
+exports.AleatoirPage = class AleatoirPage {
+    
+
+    constructor(page) {
+        this.page = page
+        this.aleatoir_buttom = page.locator("xpath=//*[contains(text(),'Aléatoire')]")
+    }
+
+
+
+    async toConnect() {
+        await this.page.goto('https://bouletcorp.com/');
+    }
+
+    async clickOnAleatoir() {
+        await this.aleatoir_buttom.first().click();
+    }
+
+    async checkUrlChanged() {
+    }
+
+    
+}
---
+++ tests/aleatoir.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+import { AleatoirPage } from '../pages/aleatoir';
+
+
+test('test', async ({ page }) => {
+    const Ale = new AleatoirPage(page)
+
+    await Ale.toConnect()
+    //await page.waitForTimeout(30000)
+    await Ale.clickOnAleatoir()
+    //await page.waitForTimeout(10000)
+    const facebookWidget = page.locator('div:nth-child(5) > #notes > #small_nav > #social_network > .socialize-this > li:nth-child(3) > a')
+    const twitterWidget = page.locator('div:nth-child(5) > #notes > #small_nav > #social_network > .socialize-this > li:nth-child(4) > a')
+    const tumblrWidget = page.locator('div:nth-child(5) > #notes > #small_nav > #social_network > .socialize-this > li:nth-child(5) > a')
+
+    facebookWidget.isVisible
+    twitterWidget.isVisible
+    tumblrWidget.isVisible
+});
